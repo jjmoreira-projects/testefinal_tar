ec680b7a3b4fde95f6e9d6f27a586209d2b7fb73
NetAcad DEVASC <changeme@example.com>
2023-12-11 11:35:57 +0000

    Teste de: Diogo Costa

---
+++ diogo_costaa/consome_api_diogo_costaa.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+api_url = "http://instituto.islagaia.pt/ws/wsCambio.asmx/cambioUSD"
+  
+response = requests.get(api_url)
+
+if response.status_code == 200:
+	try:
+		data = response.json()
+		valor_cambio = data["value"]
+
+		print (f"Valor do câmbio:
+{valor_cambio}")
+	except json.JSONDecodeError:
+	print ("Erro ao descodificar a resposta JSON.")
+else:
+
+	
+else:
+		print (f"Falha na solicitação à API.Código de status: {response.status_code}")
---
+++ diogo_costaa/equipamentos_primeiro_ultimo.py
@@ -0,0 +1,21 @@
+with open('devices.txt', 'r') as file:
+
+equipamentos = [equipamento.strip() for equipamento in file.readlines()]
+
+while True:
+	equipamento_procurar = input("Equipamento a procura (sair p/ terminal): ")
+
+	if equipamento_procurar.lower() == 'sair':
+								break 
+
+	equipamentos_encontrados = [eq for eq
+in equipamentos if
+equipamento_procurar.lower() in eq.lower()]
+
+	if equipamentos_encontrados:
+		print (f"Equipamentos encontrados:
+{','.join(equipamentos_encontrados)}")
+		print (f"Quantidade:
+{len(equipamentos_encontrados)}")
+	else:
+		print("Equipamento Não-existe na Lista!")
