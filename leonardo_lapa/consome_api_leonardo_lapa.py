8dc194e7da9be806373609abb8345bbca2495101
leonardolapa <laomaoshiao@gmail.com>
2023-12-11 10:57:13 +0000

    teste de: leonardo lapa

---
+++ leonardo_lapa/consome_api_leonardo_lapa.py
@@ -0,0 +1,12 @@
+import requests
+import xml.etree.ElementTree as ET
+
+
+response = requests.get("http://instituto.islagaia.pt/ws/wscambio.asmx/cambioUSD")
+print(response.text)
+print()
+print(response.content)
+root = ET.fromstring(response.content)
+print(root.text)
+
+input("Press enter to exit;")
