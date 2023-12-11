c887a6d88f2fd9ba9570ddb90be47c54dd8f3e3f
NetAcad DEVASC <changeme@example.com>
2023-12-11 10:15:32 +0000

    Teste de: Bruno_Barbosa

---
+++ Bruno_Barbosa/consome_api_Bruno_Barbosa.py
@@ -0,0 +1,9 @@
+import requests
+import xml.etree.ElementTree as ET
+resp = requests.get("http://instituto.islagaia.pt/ws/wsCambio.asmx/cambioUSD")
+print(resp)
+print()
+print(resp.content)
+root = ET.fromstring(resp.content)
+print('Cambio USD:',root.text)
+
